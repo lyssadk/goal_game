ca1a93dcf980756b1dddce5b25faea3c58591bc2
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-03-20 16:14:28 -0600

    reacttest

--- goalGame/main.js
+++ goalGame/main.js
@@ -19,14 +19,14 @@ camera.position.y=1.5;
 
 // Ground
 const ground = new THREE.Mesh(
-  new THREE.BoxGeometry( 10, 1, 10 ),
+  new THREE.BoxGeometry( 2, 1, 1 ),
   new THREE.MeshPhongMaterial( { color: 0x00aaaa } )
 ); 
 ground.position.y = -1;
 scene.add( ground ); 
 
 // world???
-const geometry = new THREE.SphereGeometry( 2, 32, 16 ); 
+const geometry = new THREE.SphereGeometry( 4, 32, 16 ); 
 const material = new THREE.MeshPhongMaterial( { color: 0xa0aaa } ); 
 const sphere = new THREE.Mesh( geometry, material ); 
 sphere.position.y = 3;
@@ -37,16 +37,24 @@ const loader = new GLTFLoader();
 loader.load('Fish.glb', (gltf) => {
   const fishDude = gltf.scene;
   fishDude.scale.set(0.5, 0.5, 0.5);
-  fishDude.position.y = 1.5;
+  // make sure the fish is on the sphere but not in the sphere lol
+  fishDude.position.set(0, 3.9, 1);
+  
   sphere.add(fishDude);
 });
-// animate the fish
+
+//animate sphere to rotate around the y axis 
+function animateSphere() {
+  requestAnimationFrame(animateSphere);
+  sphere.rotation.y += .0001;
+}
+
 
 // Lighting
   const hemLight = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
   scene.add( hemLight );
   const color = 0xFFFFFF;
-  const intensity = 1;
+  const intensity = 1.3;
   const light = new THREE.DirectionalLight(color, intensity);
   light.position.set(0, 1,-1);
   scene.add(light);
@@ -58,7 +66,7 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 // actually render the scene and call all the fun stuff
 function animate() {
-
+  animateSphere();
   requestAnimationFrame( animate );
   controls.update();
   renderer.render( scene, camera );
